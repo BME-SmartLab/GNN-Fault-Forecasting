23615b023cfb8fc02f8429660f36008a5e0e1f58
daniel-unyi-42 <63173826+daniel-unyi-42@users.noreply.github.com>
2023-11-10 18:29:40 +0100

    Argument fixes in Explainer.py

--- Explainer.py
+++ Explainer.py
@@ -55,12 +55,12 @@ class ActorGNN(nn.Module):
         x = self.conv4(x, data.edge_index)
         return torch.sigmoid(x)
 
-    def train_batch(self, loader, critic, baseline):
+    def train_batch(self, loader):
         actor_losses = []
         mask_sizes = []
-        critic_pos_maes = []
-        critic_neg_maes = []
-        baseline_maes = []
+        critic_pos_accs = []
+        critic_neg_accs = []
+        baseline_accs = []
         for data in loader:
             data = data.to(self.device)
             self.eval()
@@ -68,17 +68,17 @@ class ActorGNN(nn.Module):
                 probs = self(data)
                 mask = torch.bernoulli(probs)
                 mask_sizes.append(torch.sum(mask).item() / len(mask))
-                critic_pos_out = critic(data, mask)
-                pos_mae = F.l1_loss(data.y, critic_pos_out)
-                critic_pos_maes.append(pos_mae.item())
-                critic_neg_out = critic(data, 1.0 - mask)
-                neg_mae = F.l1_loss(data.y, critic_neg_out)
-                critic_neg_maes.append(neg_mae.item())
-                baseline_out = baseline(data)
-                baseline_mae = F.l1_loss(data.y, baseline_out)
-                baseline_maes.append(baseline_mae.item())
-                critic_pos_loss = critic.criterion(data.y, critic_pos_out, reduction='none')[data.batch]
-                baseline_loss = baseline.criterion(data.y, baseline_out, reduction='none')[data.batch]
+                critic_pos_out = self.critic(data, mask)
+                pos_acc = (critic_pos_out.argmax(dim=1) == data.y).sum() / data.y.size(0)
+                critic_pos_accs.append(pos_acc.item())
+                critic_neg_out = self.critic(data, 1.0 - mask)
+                neg_acc = (critic_neg_out.argmax(dim=1) == data.y).sum() / data.y.size(0)
+                critic_neg_accs.append(neg_acc.item())
+                baseline_out = self.baseline(data)
+                baseline_acc = (baseline_out.argmax(dim=1) == data.y).sum() / data.y.size(0)
+                baseline_accs.append(baseline_acc.item())
+                critic_pos_loss = self.critic.criterion(critic_pos_out, data.y, reduction='none')[data.batch]
+                baseline_loss = self.baseline.criterion(baseline_out, data.y, reduction='none')[data.batch]
                 reward = -(critic_pos_loss - baseline_loss)
             self.train()
             self.optimizer.zero_grad()
@@ -89,45 +89,45 @@ class ActorGNN(nn.Module):
             actor_losses.append(loss.item())
         return sum(actor_losses)/len(actor_losses),\
             sum(mask_sizes)/len(mask_sizes),\
-                sum(critic_pos_maes)/len(critic_pos_maes),\
-                    sum(critic_neg_maes)/len(critic_neg_maes),\
-                        sum(baseline_maes)/len(baseline_maes)
+                sum(critic_pos_accs)/len(critic_pos_accs),\
+                    sum(critic_neg_accs)/len(critic_neg_accs),\
+                        sum(baseline_accs)/len(baseline_accs)
 
     @torch.no_grad()
-    def test_batch(self, loader, critic, baseline):
+    def test_batch(self, loader):
         actor_losses = []
         mask_sizes = []
-        critic_pos_maes = []
-        critic_neg_maes = []
-        baseline_maes = []
+        critic_pos_accs = []
+        critic_neg_accs = []
+        baseline_accs = []
         self.eval()
         for data in loader:
             data = data.to(self.device)
             probs = self(data)
             mask = (probs > 0.5).float()
-            critic_pos_out = critic(data, mask)
-            pos_mae = F.l1_loss(data.y, critic_pos_out)
-            critic_pos_maes.append(pos_mae.item())
-            critic_neg_out = critic(data, 1.0 - mask)
-            neg_mae = F.l1_loss(data.y, critic_neg_out)
-            critic_neg_maes.append(neg_mae.item())
+            critic_pos_out = self.critic(data, mask)
+            pos_acc = (critic_pos_out.argmax(dim=1) == data.y).sum() / data.y.size(0)
+            critic_pos_accs.append(pos_acc.item())
+            critic_neg_out = self.critic(data, 1.0 - mask)
+            neg_acc = (critic_neg_out.argmax(dim=1) == data.y).sum() / data.y.size(0)
+            critic_neg_accs.append(neg_acc.item())
             mask_sizes.append(torch.sum(mask).item() / len(mask))
-            baseline_out = baseline(data)
-            baseline_mae = F.l1_loss(data.y, baseline_out)
-            baseline_maes.append(baseline_mae.item())
-            critic_pos_loss = critic.criterion(data.y, critic_pos_out, reduction='none')[data.batch]
-            baseline_loss = baseline.criterion(data.y, baseline_out, reduction='none')[data.batch]
+            baseline_out = self.baseline(data)
+            baseline_acc = (baseline_out.argmax(dim=1) == data.y).sum() / data.y.size(0)
+            baseline_accs.append(baseline_acc.item())
+            critic_pos_loss = self.critic.criterion(critic_pos_out, data.y, reduction='none')[data.batch]
+            baseline_loss = self.baseline.criterion(baseline_out, data.y, reduction='none')[data.batch]
             reward = -(critic_pos_loss - baseline_loss)
             loss = self.loss(reward, mask, probs)
             actor_losses.append(loss.item())
         return sum(actor_losses)/len(actor_losses),\
             sum(mask_sizes)/len(mask_sizes),\
-                sum(critic_pos_maes)/len(critic_pos_maes),\
-                    sum(critic_neg_maes)/len(critic_neg_maes),\
-                        sum(baseline_maes)/len(baseline_maes)
+                sum(critic_pos_accs)/len(critic_pos_accs),\
+                    sum(critic_neg_accs)/len(critic_neg_accs),\
+                        sum(baseline_accs)/len(baseline_accs)
     
     @torch.no_grad()
-    def predict_batch(self, loader, critic, baseline):
+    def predict_batch(self, loader):
         self.eval()
         y_probs = []
         y_masks = []
